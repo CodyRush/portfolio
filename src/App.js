be5fac8ed23db1234bec2a7554f5ce294eab15e1
Cody Rush <rush_cody@hotmail.com>
2020-02-04 13:56:13 -0700

    styled header and navbar for mobile

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import NavBar from './Components/NavBar';
+import Header from './Components/Header';
+
+import 'minireset.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <NavBar />
+      <Header />
+    </div>
+  );
+}
+
+export default App;
